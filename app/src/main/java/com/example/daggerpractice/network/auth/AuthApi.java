4b02b80d2a80df72ba2faca6e95dd03a8ec4e30b
izadorozhnyi <izadorozhnyi@vibrenthealth.com>
2019-06-21 23:19:42 -0400

    *initial successful api cal and response converted into User object

--- app/src/main/java/com/example/daggerpractice/network/auth/AuthApi.java
+++ app/src/main/java/com/example/daggerpractice/network/auth/AuthApi.java
@@ -1,8 +1,10 @@
 package com.example.daggerpractice.network.auth;
 
+import com.example.daggerpractice.models.User;
+
 import io.reactivex.Flowable;
-import okhttp3.ResponseBody;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 // scope of this interface should only be within Auth module
 public interface AuthApi {
@@ -10,7 +12,7 @@ public interface AuthApi {
 
 //    1. Return Flawable object (Rx object)
 //    2. Inside ViewModel we can convert Flowable into LiveData object
-    @GET
-    Flowable<ResponseBody> getResponseObject();
-
+    @GET("users/{id}") // <-- this tells us which call will result getting one User object from the endpoint, ex. users/4
+    Flowable<User> getUser(@Path("id") int id); // <-- this needs to match @GET above
 }
+
--- app/src/main/java/com/example/daggerpractice/ui/auth/AuthViewModel.java
+++ app/src/main/java/com/example/daggerpractice/ui/auth/AuthViewModel.java
@@ -4,9 +4,14 @@ import android.util.Log;
 
 import androidx.lifecycle.ViewModel;
 
+import com.example.daggerpractice.models.User;
 import com.example.daggerpractice.network.auth.AuthApi;
 
 import javax.inject.Inject;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class AuthViewModel extends ViewModel {
 
@@ -28,6 +33,35 @@ public class AuthViewModel extends ViewModel {
             Log.d(TAG, "AuthViewModel: AuthApi is PRESENT and NOT NULL");
         }
 //        =============================
+
+//        test request to check retrofit hitting api and getting User object below
+
+        authApi.getUser(1)
+                .toObservable()
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Observer<User>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        
+                    }
+
+                    @Override
+                    public void onNext(User user) {
+                        Log.d(TAG, "onNext from AuthApi: should be background thread " + Thread.currentThread().getName());
+                        Log.d(TAG, "onNext from AuthApi: users email address was retrieved: " + user.getEmail());
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "onError from AuthApi: " + e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
+
     }
 
 }
