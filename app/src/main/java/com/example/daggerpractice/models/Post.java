15d48854a5d8724b35bd77a75a5f1589c80d3d95
izadorozhnyi <izadorozhnyi@vibrenthealth.com>
2019-07-06 12:23:00 -0400

    * added MainApi, Post model, MainModule * added module to ActivityBuildersModule to be accessible within Main conponent as submodule for injection

---
+++ app/src/main/java/com/example/daggerpractice/models/Post.java
@@ -0,0 +1,65 @@
+package com.example.daggerpractice.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+    @SerializedName("userId")
+    @Expose
+    private int userId;
+
+    @SerializedName("id")
+    @Expose
+    private int id;
+
+    @SerializedName("title")
+    @Expose
+    private String title;
+
+    @SerializedName("body")
+    @Expose
+    private String body;
+
+    public Post(int userId, int id, String title, String body) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public Post() {
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+}
