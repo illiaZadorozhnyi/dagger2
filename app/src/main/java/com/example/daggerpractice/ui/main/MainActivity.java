033dcf0ec1dcff14e33b2375f005d362a6f45efc
izadorozhnyi <izadorozhnyi@vibrenthealth.com>
2019-07-06 22:01:18 -0400

    * added drawer_view layout * modified activity_main layout to include DrawerLayout and NavigationView * implemented I from MainActivity and started implementing onNavigationItemSelected

--- app/src/main/java/com/example/daggerpractice/ui/main/MainActivity.java
+++ app/src/main/java/com/example/daggerpractice/ui/main/MainActivity.java
@@ -1,27 +1,35 @@
 package com.example.daggerpractice.ui.main;
 
-import android.content.ClipData;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.daggerpractice.BaseActivity;
 import com.example.daggerpractice.R;
 import com.example.daggerpractice.ui.main.posts.PostsFragment;
-import com.example.daggerpractice.ui.main.profile.ProfileFragment;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "MainActivity";
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
 
         Log.d(TAG, "onCreate: inside onCreate of the MainActivity...");
 
@@ -52,6 +60,26 @@ public class MainActivity extends BaseActivity {
                 .replace(R.id.main_container, new PostsFragment())
                 .commit();
         Log.d(TAG, "testFragment: inside Posts Fragment...");
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.nav_profile: {
+
+                break;
+            }
+            case R.id.nav_posts: {
+
+                break;
+            }
+        }
+
+        item.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
 
